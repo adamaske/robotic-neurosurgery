ffc319e79af9c3109c6302b3d729805e92077919
Adam <adamaske@live.no>
2024-11-14 11:39:30 +0100

    testing

--- ws/src/test_package/launch/listener.launch.py
+++ ws/src/test_package/launch/listener.launch.py
@@ -2,4 +2,4 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([ Node(packge='demo_nodes_cpp', executable='listener')])
+    return LaunchDescription([ Node(package='demo_nodes_cpp', executable='listener')])
--- ws/src/test_package/launch/talker.launch.py
+++ ws/src/test_package/launch/talker.launch.py
@@ -2,4 +2,4 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([ Node(packge='demo_nodes_cpp', executable='talker')])
+    return LaunchDescription([ Node(package='demo_nodes_cpp', executable='talker')])
