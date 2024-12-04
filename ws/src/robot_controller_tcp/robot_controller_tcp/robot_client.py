4fe14ac66046552af9e06a6108d70349ce211fd8
Adam Aske <adamaske@live.no>
2024-12-04 03:13:35 +0100

    we

--- ws/src/robot_controller_tcp/robot_controller_tcp/robot_client.py
+++ ws/src/robot_controller_tcp/robot_controller_tcp/robot_client.py
@@ -4,12 +4,62 @@ from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 import socket 
 import struct
+import rclpy.time
+from sensor_msgs.msg import JointState 
+from std_msgs.msg import Header
+#from geometry_msgs.msg import TransformStamped, Transform
+from tf2_ros.transform_broadcaster import TransformBroadcaster
+from tf2_geometry_msgs.tf2_geometry_msgs import Pose
+import moveit_commander
+import sys
 
 class robot_client(Node):
     def __init__(self):
         super().__init__("RobotControllerTCPClient")
         self.get_logger().info("robot_client: Welcome!")
 
+        self.joint_publisher = self.create_publisher(JointState, 'joint_states', 10)
+
+        moveit_commander.roscpp_initalize(sys.argv)
+        self.group = moveit_commander.MoveGroupCommander("move_group")
+
+        self.connect()
+        self.run_client()
+
+    def publish_joint_angles(self, angles):
+        joint_goal = self.group.get_current_joint_values()
+        joint_goal[0] = 0
+        joint_goal[1] = 0
+        joint_goal[2] = 0
+        joint_goal[3] = 0
+        joint_goal[4] = 0
+        joint_goal[5] = 0
+        joint_goal[6] = 0
+        
+        self.group.go(joint_goal, wait=True)
+
+        self.group.stop()
+
+        current_joints = self.group.get_current_joint_values()
+        return current_joints
+
+        self.publish_setup()
+        msg = JointState()
+        msg.header = Header()
+        msg.name = ["shoulder_pan_joint", 
+                    "shoulder_lift_joint", 
+                    "elbow_joint", 
+                    "wrist_1_joint",
+                    "wrist_2_joint",
+                    "wrist_3_joint"]
+        msg.position = angles
+        msg.velocity = []
+        msg.effort = []
+        self.joint_publisher.publish(msg=msg)
+        print(f"robot_client : Published to 'joint_states' -> {angles}")
+
+
+    def connect(self):
         self.server_host = "192.168.50.53" # host ip
         self.server_port = 65432 # host port
         print(f"robot_client : Client connecting to {self.server_host}:{self.server_port}")
@@ -17,7 +67,6 @@ class robot_client(Node):
         self.client_socket.connect((self.server_host, self.server_port)) # wait for accepted connection
         print(f"robot_client : Client connected succesfully to {self.server_host}:{self.server_port}")
         self.connected = True
-        self.run_client()
 
     def run_client(self): #listens to host port
         try:
@@ -31,12 +80,10 @@ class robot_client(Node):
                     break
                 print(f"robot_client : Received {byte_data}")
                 float_array = struct.unpack("<6f" , byte_data)
-                thetas = float_array
-                for i in range(len(thetas)):
-                    print(f"Theta {i} : {thetas[i]}")
 
-                    
-                print(f"Received floats: {float_array}")
+                self.publish_joint_angles(float_array)
+                
+
 
             self.client_socket.close()   
             print("robot_client : Closed client")     
